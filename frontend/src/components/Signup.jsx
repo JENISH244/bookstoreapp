cb6ff74f0d618f528fc828d3a49e54438f963e76
JENISH244 <jbsojitra03@gmail.com>
2025-03-03 18:53:08 +0530

    URL Change

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -23,7 +23,7 @@ export default function Signup() {
             password: data.password,
         }
         await axios
-            .post("http://localhost:4001/user/signup", userInfo)
+            .post("https://bookstoreapp-backend-zvfh.onrender.com/user/signup", userInfo)
             .then((res) => {
                 console.log("res.data ", res.data)
                 if (res.data) {
